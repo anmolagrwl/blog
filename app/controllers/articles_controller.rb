7db785aa02da08059cfdc9c7fad11c77b761e074
Anmol Agrawal <A2@Anmols-MacBook-Pro.local>
2014-07-17 22:54:01 -0700

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,46 @@
+class ArticlesController < ApplicationController
+	def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article, notice: "Article was successfully created."
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update_attributes(article_params)
+      redirect_to @article, notice: "Article was successfully updated."
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_url
+  end
+
+  private
+  	def article_params
+  		params.require(:article).permit(:name, :content)
+  	end
+end
