ea3b2ca04d814f5f6ed4e9a48cbe67fd90349701
Anmol Agrawal <anmol1771@gmail.com>
2017-07-14 03:44:00 +0530

    added semantic logger

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,11 @@ module Blog
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.rails_semantic_logger.started    = true
+    config.rails_semantic_logger.processing = true
+    config.rails_semantic_logger.rendered   = true
+    config.semantic_logger.backtrace_level = :info
+    config.rails_semantic_logger.quiet_assets = true
+    config.colorize_logging = true
   end
 end
