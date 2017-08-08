12cdc9e0e57a9d1a2de0d4d62ea5abb695c6504c
Anmol Agrawal <anmol1771@gmail.com>
2017-08-08 13:46:54 +0530

    semantic logger working fine with STDOUT and pushing to sematext

--- README.md
+++ README.md
@@ -15,3 +15,10 @@ docker-compose start
 docker-compose stop
 
 https://rossfairbanks.com/2015/03/06/rails-app-on-docker-using-passenger-image.html
+
+2017-08-08 05:35:44.922449 I [461:6115540] (3.878ms) ArticlesController -- Completed #show -- { :controller => "ArticlesController", :action => "show", :params => { "id" => "2" }, :format => "HTML", :method => "GET", :path => "/articles/2", :status => 200, :view_runtime => 1.83, :db_runtime => 0.72, :status_message => "OK" }
+
+su postgres
+psql postgres
+https://www.codementor.io/devops/tutorial/getting-started-postgresql-server-mac-osx
+http://www.justinweiss.com/articles/keeping-your-logs-from-becoming-an-unreadable-mess/
--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+require 'semantic_logger'
+
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
@@ -10,7 +12,7 @@ module Blog
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+# -- all .rb files in that directory are automatically loaded.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
@@ -19,12 +21,8 @@ module Blog
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.logger = Logger.new(STDOUT)
-
-    # config.lograge.enabled = true
-    # config.lograge.formatter = Lograge::Formatters::Logstash.new
-
-    config.rails_semantic_logger.semantic   = true
+    
+    SemanticLogger.add_appender(io: STDOUT)
 
   end
 end
